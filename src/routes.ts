92f857c58bd7a1ad514a1f73523b43bab4acad81
Kenny Lai <kennylai360@gmail.com>
2023-10-30 21:48:11 +0000

    Inital commit for netlify deploy

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { CrisisListComponent } from "./app/crisis-list/crisis-list.component";
+import { HeroesListComponent } from "./app/heroes-list/heroes-list.component";
+
+export const routes = [
+    {path: 'crisis-list', component: CrisisListComponent},
+    {path: 'heroes-list', component: HeroesListComponent}
+]
